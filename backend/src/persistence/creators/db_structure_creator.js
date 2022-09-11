0846a07f4ee8e24adacf945086da8877f20d3bf1
Murilo <mumsm@hotmail.com>
2022-09-10 21:02:24 -0300

    suschainable

--- backend/src/persistence/creators/db_structure_creator.js
+++ backend/src/persistence/creators/db_structure_creator.js
@@ -37,7 +37,21 @@ db.serialize(() => {
     
     db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (2, 'Blue Coat', 'Fashion Coat', 200, 1, 0)"); 
     
-    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (3, 'Green Coat', 'Fashion Coat', 300, 2, 0)"); 
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (3, 'Green Coat', 'Fashion Coat', 300, 2, 0)");
+    
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (4, 'Black Coat', 'Fashion Coat', 400, 3, 0)");
+    
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (5, 'Dogs Cookies', 'Feed the dogs', 500, 4, 0)");
+    
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (6, 'Skate', 'Become Tony Hawk', 3000, 5, 0)"); 
+    
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (7, 'Eletric Bike', 'For lazy people', 5000, 6, 0)"); 
+    
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (8, 'Shades', 'I'm Batman', 15000, 7, 0)"); 
+    
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (9, 'Bulletproof Coat', 'Always prepared', 50000, 8, 0)"); 
+    
+    db.run("INSERT INTO nfts (id, title, description, price, smartContractIndex, owner_user_id) VALUES (10, 'Hekmet', 'Unlock 100% of Cerebral Actions', 100000, 9, 0)"); 
 
     db.each("SELECT * FROM nfts", (err, row) => {
         console.log(row);
